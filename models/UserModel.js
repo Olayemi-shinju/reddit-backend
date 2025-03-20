f76c7108b481eaae33ca7b40cc2d8036e9eb7811
OLAYEMI <awoyemiolayemi27@gmail.com>
2025-03-20 22:11:24 +0100

    made changes

--- models/UserModel.js
+++ models/UserModel.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 const schema = mongoose.Schema
-
+const bcrypt = require('bcrypt')
 const userSchema = new schema({
     fullname: {type: String, required: [true, 'Full Name Required']},
     email: {type: String, required: [true, 'Email Required'], unique: true},
@@ -11,6 +11,15 @@ const userSchema = new schema({
     description: {type: String, default: null},
     date_created: {type: Date, default: Date.now()}
 })
+
+userSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) return next();
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
+  });
+  userSchema.methods.comparePassword = function (enteredPassword) {
+    return bcrypt.compare(enteredPassword, this.password);
+  };
 
 const User = mongoose.model('User', userSchema)
 module.exports = User
